9b33cb57ca22a153cf641b9a9595b8b252d2ef25
Alex Gonzalez <alex.gonzalezdiaz@snhu.edu>
2023-06-30 16:24:41 -0500

    Add project files.

---
+++ ConversionType/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter a Value to !");
+string userData = Console.ReadLine();
+int userInt = Convert.ToInt32(userData);
+//long userLong = Convert.Tolong; ;
+float userFloat = (float)Convert.ToDecimal(userInt,3);
+Console.WriteLine("Here is the value as an Int: {0}", userInt);
+Console.WriteLine("Here is the value as an Float: {0}", userFloat);
