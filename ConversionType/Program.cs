00494baaa85c5154aad9416b5b168107f86a7247
AlexGonzalezDiaz <138159711+AlexGonzalezDiaz@users.noreply.github.com>
2023-07-06 13:31:39 -0500

    Changed the comments for this code

--- ConversionType/Program.cs
+++ ConversionType/Program.cs
@@ -1,6 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Program that takes the users numbers and converts them to boolean values.
+
+
 Console.Write("Enter a number to convert into a boolean!  \r\n");
 String userData = Console.ReadLine();
 int userInt = Convert.ToInt32(userData);
-bool userBool = Convert.ToBoolean(userInt);
-Console.WriteLine("Here is the value as an Boolean: {0}", userBool);
+bool newBool = Convert.ToBoolean(userInt);
+Console.WriteLine("Here is the value as an Boolean: {0}", newBool);
